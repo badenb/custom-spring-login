f589ac9a1ba512e6bc7d0ecbf006c045e0c5d0e9
Baden <baden1998@gmail.com>
2024-11-20 15:18:24 -0600

    initial commit

---
+++ src/test/java/org/example/customspringlogin/CustomSpringLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.customspringlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assert true;
+    }
+}
